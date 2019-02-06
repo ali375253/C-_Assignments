5c7e37403929dcde902e49731cd0ec14463b0297
Aliasgar Aabadani <aliasagar@promactinfo.com>
2019-02-06 17:35:06 +0530

    Assignment 1 Uploaded.

---
+++ Assignment 1/Assignment1/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Employee
+    {
+        public Employee(int Id,string Name,string DepartmentName)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.DepartmentName = DepartmentName;
+        }
+        public int GetId()
+        {
+            MethodCalled("GetId()");
+            return Id;
+        }
+        public string GetName()
+        {
+            MethodCalled("GetName()");
+            return Name;
+        }
+        public string GetDepartmentName()
+        {
+            MethodCalled("GetDepartmentName()");
+            return DepartmentName;
+        }
+        public void UpdateEmployee(int Id)
+        {
+            MethodCalled("UpdateEmployee()");
+            this.Id = Id;
+        }
+        public void UpdateEmployee(int Id,string Name)
+        {
+            MethodCalled("UpdateEmployee()");
+            this.Id = Id;
+            this.Name = Name;
+        }
+        public void UpdateEmployee(int Id,string Name,string DepartmentName)
+        {
+            MethodCalled("UpdateEmployee()");
+            this.Id = Id;
+            this.Name = Name;
+            this.DepartmentName = DepartmentName;
+        }
+
+        private int Id { get; set; }
+        private string Name { get; set; }
+        private string DepartmentName { get; set; }
+        public event MethodCalledDelegate MethodCalled;
+    }
+}
---
+++ Assignment 1/Assignment1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Id : ");
+            int Id= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Name : ");
+            string Name = Console.ReadLine();
+   
+            Console.WriteLine("Enter the Deparatment Name : ");
+            string DepartmentName = Console.ReadLine();
+
+            Employee employee = new Employee(Id,Name,DepartmentName);
+            employee.MethodCalled += OnMethodCalled;
+
+            Console.WriteLine("Id : " + employee.GetId());
+            Console.WriteLine("Name : " + employee.GetName());
+            Console.WriteLine("Department Name : " + employee.GetDepartmentName());
+
+            //Updating Employee Details
+            employee.UpdateEmployee(12,"Aliasgar","Development");
+
+            //Displaying Updated Details
+            Console.WriteLine("Id : " + employee.GetId());
+            Console.WriteLine("Name : " + employee.GetName());
+            Console.WriteLine("Department Name : " + employee.GetDepartmentName());
+        }
+
+        static void OnMethodCalled(string MethodName)
+        {
+            Console.WriteLine($"{ MethodName} method called");
+        }
+    }
+}
